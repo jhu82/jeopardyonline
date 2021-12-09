264a0a6ffb2f7626ec3a7f5b83d7d0da22cc556c
Jinming Hu <jmh.51193@gmail.com>
2021-12-08 21:09:28 -0800

    Added preliminary logic for interfacing with j-service API, for rendering Jeopardy questions

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const io = require("socket.io")({
         credentials: true
     },
 });
+const axios = require("axios");
 
 io.attach(http, {
     pingInterval: 10000,
@@ -21,7 +22,23 @@ io.on('connection', socket => {
     io.emit('message', { name, message })
     console.log(message);
   })
+
+  socket.on('initialize', async () => {
+    const seed = Math.floor(1000 + Math.random() * 15000);
+    console.log(seed);
+    //Grab 5 random categories based on seed number
+    const url = `https://jservice.io/api/categories?count=5&offset=${seed}`
+    try {
+        console.log('hello');
+        const categories = await axios.get(url);
+        console.log(categories);
+        io.emit('categories', categories.data);
+    }catch(e) {
+        console.log(e);
+    }
+  })
 })
+
 
 http.listen(4000, function() {
   console.log('listening on port 4000')
