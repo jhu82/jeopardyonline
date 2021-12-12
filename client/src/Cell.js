a2f5c5657c17a29b81826c31a17b8a5156b11ae3
Jinming Hu <jmh.51193@gmail.com>
2021-12-11 22:13:26 -0800

    Added some logic for handling clicking on each Cell, for question to be displayed

--- client/src/Cell.js
+++ client/src/Cell.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Question from './Question.js';
 import styles from './Cell.module.css';
 
-export default function Cell({ name }) {
+export default function Cell({ question, value }) {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(true);
+    }
     return (
-        <div className={styles['cell']}>
-            <h1>{name}</h1>
+        <div className={styles['cell']} onClick={handleClick()}>
+            <h1>{value}</h1>
+            {clicked && <Question question={question} />}
         </div>
     )
 }
